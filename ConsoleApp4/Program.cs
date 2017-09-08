cb6247ac0fca12cd67154e4deed94b3258d503a9
Thierry Koppenol <thierry.koppenol@mybit.nl>
2017-09-08 17:52:41 +0200

    Added Calc project

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,54 @@
+﻿using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static int count = 0;
+
+        static void Main(string[] args)
+        {
+            // Reconize User input (+,-,/,*) and give result
+
+            Console.WriteLine("Welcome to the calculator");
+            Console.WriteLine(count);
+            try
+            {
+                while (true)
+                {
+                    var ReadLine = Console.ReadLine();
+                    string operation = ReadLine.Substring(0, 1);
+                    int value = int.Parse(ReadLine.Substring(1));
+                    switch (operation)
+                    {
+                        case "+":
+                            count = Operations.Addition(value, count);
+                            break;
+                        case "-":
+                            count = Operations.Minus(value, count);
+                            break;
+                        case "/":
+                            count = Operations.Divide(value, count);
+                            break;
+                        case "*":
+                            count = Operations.Multiply(value, count);
+                            break;
+                        default:
+                            Console.WriteLine("This is not a valid operation");
+                            break;
+                    }
+                }
+            }
+            catch (DivideByZeroException e) {
+                Console.WriteLine("Can't divide by zero");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = 232;
+            var b = 23;
+            var c = Operations.Addition(a, b);
+            Assert.IsTrue(c > 1);
+        }
+    }
+}
