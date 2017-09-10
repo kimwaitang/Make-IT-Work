40560adb78c601c87137e56c9c2f1c5f70560612
Kim Wai Tang <Kim.Wai.Tang@MyBit.nl>
2017-09-10 18:51:03 +0200

    Add second example

---
+++ UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Data;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Mock<IDbConnection> connection = new Mock<IDbConnection>();
+            connection.Setup(x => x.Open()).Callback(this.Open);
+            connection.Object.Open();
+        }
+
+        public void Open()
+        {
+
+        }
+    }
+}
